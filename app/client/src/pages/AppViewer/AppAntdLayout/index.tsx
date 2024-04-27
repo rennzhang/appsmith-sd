04e7d251cd467e559e50e1f1ebf7386c1ed07ec8
Renkin <906155099@qq.com>
2024-04-27 15:05:22 +0800

    feat: lu'yz路由标签功能

--- app/client/src/pages/AppViewer/AppAntdLayout/index.tsx
+++ app/client/src/pages/AppViewer/AppAntdLayout/index.tsx
@@ -1,38 +1,14 @@
-import {
-  CaretDownFilled,
-  DoubleRightOutlined,
-  GithubFilled,
-  InfoCircleFilled,
-  LogoutOutlined,
-  PlusCircleFilled,
-  QuestionCircleFilled,
-  SearchOutlined,
-  CrownFilled,
-} from "@ant-design/icons";
-import ReactDOM from "react-dom";
-import { Icon } from "@blueprintjs/core";
+import { LogoutOutlined } from "@ant-design/icons";
 import { builderURL, viewerURL } from "RouteBuilder";
-
 import type { ProSettings } from "@ant-design/pro-components";
 import {
   PageContainer,
-  ProCard,
   ProConfigProvider,
   ProLayout,
   SettingDrawer,
 } from "@ant-design/pro-components";
-import css from "@emotion/css";
-import {
-  Button,
-  ConfigProvider,
-  Divider,
-  Dropdown,
-  Input,
-  Popover,
-  theme,
-} from "antd";
-import React, { useEffect, useMemo, useState } from "react";
-import defaultProps from "./_defaultProps";
+import { ConfigProvider, Divider, Dropdown, theme } from "antd";
+import { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import {
   getCurrentApplication,
@@ -52,70 +28,9 @@ import { mapClearTree } from "utils/treeUtils";
 import { makeRouteNode } from "../utils";
 import { getAppMode } from "selectors/entitiesSelector";
 
-const Item: React.FC<{ children: React.ReactNode }> = (props) => {
-  const { token } = theme.useToken();
-  return (
-    <div
-      className={css`
-        color: ${token.colorTextSecondary};
-        font-size: 14px;
-        cursor: pointer;
-        line-height: 22px;
-        margin-bottom: 8px;
-        &:hover {
-          color: ${token.colorPrimary};
-        }
-      `}
-      style={{
-        width: "33.33%",
-      }}
-    >
-      {props.children}
-      <DoubleRightOutlined
-        style={{
-          marginInlineStart: 4,
-        }}
-      />
-    </div>
-  );
-};
-
-const List: React.FC<{ title: string; style?: React.CSSProperties }> = (
-  props,
-) => {
-  const { token } = theme.useToken();
-
-  return (
-    <div
-      style={{
-        width: "100%",
-        ...props.style,
-      }}
-    >
-      <div
-        style={{
-          fontSize: 16,
-          color: token.colorTextHeading,
-          lineHeight: "24px",
-          fontWeight: 500,
-          marginBlockEnd: 16,
-        }}
-      >
-        {props.title}
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-        }}
-      >
-        {new Array(6).fill(1).map((_, index) => {
-          return <Item key={index}>具体的解决方案-{index}</Item>;
-        })}
-      </div>
-    </div>
-  );
-};
+import type { MyRoute } from "./components/navList";
+import PageNavList from "./components/navList";
+import React from "react";
 
 const MenuCard = (viewerLayout: any) => {
   const { token } = theme.useToken();
@@ -135,33 +50,6 @@ const MenuCard = (viewerLayout: any) => {
     </div>
   );
 };
-
-// const SearchInput = () => {
-//   const { token } = theme.useToken();
-//   return (
-//     <div
-//       aria-hidden
-//       key="SearchOutlined"
-//       onMouseDown={(e) => {
-//         e.stopPropagation();
-//         e.preventDefault();
-//       }}
-//       style={{
-//         display: "flex",
-//         alignItems: "center",
-//         marginInlineEnd: 24,
-//       }}
-//     >
-
-//       <PlusCircleFilled
-//         style={{
-//           color: token.colorPrimary,
-//           fontSize: 24,
-//         }}
-//       />
-//     </div>
-//   );
-// };
 
 type SidebarProps = {
   currentApplicationDetails?: ApplicationPayload;
@@ -186,7 +74,7 @@ export default (props: SidebarProps) => {
     history.push(path);
   };
   const [pathname, setPathname] = useState(location.pathname);
-  const [num, setNum] = useState(40);
+  const [currentRoute, setCurrentRoute] = useState<MyRoute>();
   if (typeof document === "undefined") {
     return <div />;
   }
@@ -265,6 +153,10 @@ export default (props: SidebarProps) => {
                   />
                 ),
               };
+              if (path.includes(pathname) && !currentRoute) {
+                res.isInitRoute = true;
+                setCurrentRoute(res);
+              }
               delete res.children;
               return res;
             },
@@ -320,35 +212,8 @@ export default (props: SidebarProps) => {
   }, [viewerLayout, pages, currentApplicationDetails]);
 
   useEffect(() => {
-    defaultProps.route.routes = initState.menudata;
-    console.log(" initState.menudata", initState.menudata);
-  }, [initState]);
-
-  useEffect(() => {
     setQuery(window.location.search);
   }, [location]);
-
-  // // 将treeData1递归转换成routes结构
-  // function processChildren(children: any) {
-  //   if (!children?.length) return [];
-  //   return children.map((child: any) => {
-  //     const res = {
-  //       ...child,
-  //       path: child.pageId
-  //         ? viewerURL({
-  //             pageId: child.pageId,
-  //           })
-  //         : child.title,
-  //       name: child.title,
-  //       icon: <Icon icon={child.icon} />,
-  //       routes: processChildren(child.children || []),
-  //     };
-  //     delete res.children;
-  //     return res;
-  //   });
-  // }
-
-  // defaultProps.route.routes = processChildren(viewerLayout.treeData);
 
   return (
     <div
@@ -363,42 +228,15 @@ export default (props: SidebarProps) => {
           getTargetContainer={() => {
             return document.getElementById("test-pro-layout") || document.body;
           }}
+          theme={{
+            components: {
+              Tag: {
+                fontSizeIcon: 14,
+              },
+            },
+          }}
         >
           <ProLayout
-            bgLayoutImgList={[
-              {
-                src: "https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png",
-                left: 85,
-                bottom: 100,
-                height: "303px",
-              },
-              {
-                src: "https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png",
-                bottom: -68,
-                right: -45,
-                height: "303px",
-              },
-              {
-                src: "https://img.alicdn.com/imgextra/i3/O1CN018NxReL1shX85Yz6Cx_!!6000000005798-2-tps-884-496.png",
-                bottom: 0,
-                left: 0,
-                width: "331px",
-              },
-            ]}
-            prefixCls="my-prefix"
-            {...defaultProps}
-            // actionsRender={(props) => {
-            //   if (props.isMobile) return [];
-            //   if (typeof window === "undefined") return [];
-            //   return [
-            //     props.layout !== "side" && document.body.clientWidth > 1400 ? (
-            //       <SearchInput />
-            //     ) : undefined,
-            //     <InfoCircleFilled key="InfoCircleFilled" />,
-            //     <QuestionCircleFilled key="QuestionCircleFilled" />,
-            //     <GithubFilled key="GithubFilled" />,
-            //   ];
-            // }}
             avatarProps={{
               src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
               size: "small",
@@ -421,6 +259,43 @@ export default (props: SidebarProps) => {
                 );
               },
             }}
+            route={{ routes: initState.menudata }}
+            // bgLayoutImgList={[
+            //   {
+            //     src: "https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png",
+            //     left: 85,
+            //     bottom: 100,
+            //     height: "303px",
+            //   },
+            //   {
+            //     src: "https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png",
+            //     bottom: -68,
+            //     right: -45,
+            //     height: "303px",
+            //   },
+            //   {
+            //     src: "https://img.alicdn.com/imgextra/i3/O1CN018NxReL1shX85Yz6Cx_!!6000000005798-2-tps-884-496.png",
+            //     bottom: 0,
+            //     left: 0,
+            //     width: "331px",
+            //   },
+            // ]}
+            prefixCls="my-prefix"
+            // actionsRender={(props) => {
+            //   if (props.isMobile) return [];
+            //   if (typeof window === "undefined") return [];
+            //   return [
+            //     props.layout !== "side" && document.body.clientWidth > 1400 ? (
+            //       <SearchInput />
+            //     ) : undefined,
+            //     <InfoCircleFilled key="InfoCircleFilled" />,
+            //     <QuestionCircleFilled key="QuestionCircleFilled" />,
+            //     <GithubFilled key="GithubFilled" />,
+            //   ];
+            // }}
+            // headerContentRender={() => {
+            //   return <ProBreadcrumb />;
+            // }}
             headerTitleRender={(logo, title, _) => {
               const defaultDom = (
                 <a>
@@ -469,11 +344,11 @@ export default (props: SidebarProps) => {
             menuItemRender={(item, dom) => (
               <div
                 onClick={() => {
-                  console.log("menuItemRender click", item || "/welcome");
-
                   setPathname(item.path || "/welcome");
-                  if (item.isPage) goToPath(item.path || "/welcome");
-                  console.log(location, pathname);
+                  if (item.isPage) {
+                    setCurrentRoute(item as any);
+                    goToPath(item.path || "/welcome");
+                  }
                 }}
               >
                 {dom}
@@ -488,10 +363,21 @@ export default (props: SidebarProps) => {
             {...settings}
           >
             <PageContainer
+              header={{
+                title: <div />,
+              }}
               token={{
-                paddingInlinePageContainerContent: num,
+                paddingBlockPageContainerContent: 4,
+                paddingInlinePageContainerContent: 24,
               }}
             >
+              {currentRoute && (
+                <PageNavList
+                  currentRoute={currentRoute}
+                  goToPath={goToPath}
+                  routes={initState.menudata}
+                />
+              )}
               <div id="ant-prolayout-container" />
               {/* {props.ContainerComp ? props.ContainerComp : null} */}
             </PageContainer>
